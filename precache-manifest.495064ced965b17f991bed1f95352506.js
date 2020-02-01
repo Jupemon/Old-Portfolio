e410566a72b7a212b5cef1176a52cf31d0159606
Juho <s6heju04@gmail.com>
2020-02-01 17:59:26 +0200

    Updates

---
+++ precache-manifest.495064ced965b17f991bed1f95352506.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23923cfec2a2a8a02c3ba2f05899fa16",
+    "url": "/Old-Portfolio/index.html"
+  },
+  {
+    "revision": "a9bd902d60a28d198fc7",
+    "url": "/Old-Portfolio/static/css/main.5baefaa9.chunk.css"
+  },
+  {
+    "revision": "91bd164e1b72623e1011",
+    "url": "/Old-Portfolio/static/js/2.43990c30.chunk.js"
+  },
+  {
+    "revision": "a9bd902d60a28d198fc7",
+    "url": "/Old-Portfolio/static/js/main.02c7b5cc.chunk.js"
+  },
+  {
+    "revision": "b440868bb3fff3b38853",
+    "url": "/Old-Portfolio/static/js/runtime~main.1ad29e9a.js"
+  },
+  {
+    "revision": "2b2c4d4156f3c0c157cd86e6f0921359",
+    "url": "/Old-Portfolio/static/media/bootsrap.2b2c4d41.png"
+  },
+  {
+    "revision": "73d646c1cc3db8573cd6cfedc0a491d0",
+    "url": "/Old-Portfolio/static/media/candy1.73d646c1.PNG"
+  },
+  {
+    "revision": "527fbef975b8ba280e3d3d530eacb671",
+    "url": "/Old-Portfolio/static/media/candy2.527fbef9.png"
+  },
+  {
+    "revision": "325cd748e66afaeadee14a29a5fb32cc",
+    "url": "/Old-Portfolio/static/media/csharp.325cd748.png"
+  },
+  {
+    "revision": "284041b3d7b7483f70b8e530f5588bd1",
+    "url": "/Old-Portfolio/static/media/css3.284041b3.png"
+  },
+  {
+    "revision": "fe8f282d38525ae7dca55259deed25ad",
+    "url": "/Old-Portfolio/static/media/devdocs1.fe8f282d.PNG"
+  },
+  {
+    "revision": "f47dab4900528397b825bb99011bff42",
+    "url": "/Old-Portfolio/static/media/devdocs2.f47dab49.PNG"
+  },
+  {
+    "revision": "063969a98d669a41b94d2ddd117efdcc",
+    "url": "/Old-Portfolio/static/media/express.063969a9.png"
+  },
+  {
+    "revision": "11fdbc719fcb17641e33e57ba78ac1de",
+    "url": "/Old-Portfolio/static/media/github.11fdbc71.png"
+  },
+  {
+    "revision": "a0610c6f25029301fd52d2095108aacf",
+    "url": "/Old-Portfolio/static/media/imgchecker1.a0610c6f.PNG"
+  },
+  {
+    "revision": "7efbed56e04c72e52671103c6e54eb37",
+    "url": "/Old-Portfolio/static/media/imgchecker2.7efbed56.PNG"
+  },
+  {
+    "revision": "531aa26ac785ced999650fd9c7e6b157",
+    "url": "/Old-Portfolio/static/media/javascript.531aa26a.png"
+  },
+  {
+    "revision": "3803ce69137f518e3e749148e550c377",
+    "url": "/Old-Portfolio/static/media/knex.3803ce69.png"
+  },
+  {
+    "revision": "8766409c5fdfdcef4abe9186920ab222",
+    "url": "/Old-Portfolio/static/media/linkittv1.8766409c.PNG"
+  },
+  {
+    "revision": "665b6aa17bcb2f32dba11f099458594b",
+    "url": "/Old-Portfolio/static/media/linkittv2.665b6aa1.PNG"
+  },
+  {
+    "revision": "46f758661588703b190fb4d2aae3847a",
+    "url": "/Old-Portfolio/static/media/nodejs.46f75866.png"
+  },
+  {
+    "revision": "ebe56e1a4d2e448c8b79cd87788c5b3e",
+    "url": "/Old-Portfolio/static/media/planb1.ebe56e1a.PNG"
+  },
+  {
+    "revision": "271ce6f40da885568833514a313c6e88",
+    "url": "/Old-Portfolio/static/media/planb2.271ce6f4.PNG"
+  },
+  {
+    "revision": "23b02553642b13c5b37c76ab9c16d8b6",
+    "url": "/Old-Portfolio/static/media/postgres.23b02553.png"
+  }
+]);
